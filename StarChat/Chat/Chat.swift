9db734c415a0527dca42cf0b430c33191e84a100
Kevin Pund <lpund04@gmail.com>
2025-03-23 15:03:13 +0100

    add Messagemodel and change Chatmodel

--- StarChat/Chat/Chat.swift
+++ StarChat/Chat/Chat.swift
@@ -1,8 +1,15 @@
 import Foundation
 
-struct Chat: Identifiable {
-	let id = UUID()
+struct Chat: Identifiable, Codable {
+	let id: UUID
 	let name: String
 	let persona: String
-	var messages: [Message]
+	var messages: [ChatMessage]
+	
+	init(id: UUID = UUID(), name: String, persona: String, messages: [ChatMessage] = []) {
+		self.id = id
+		self.name = name
+		self.persona = persona
+		self.messages = messages
+	}
 }
